616f52425856bacd7df037f2f7464ffac4446d70
Junjie Huang <acmhjj@gmail.com>
2021-03-26 01:42:33 +0800

    20. Valid Parentheses

---
+++ algorithms/cpp/0020.valid-parentheses.cc
@@ -0,0 +1,55 @@
+// Source : https://leetcode.com/problems/valid-parentheses/
+// Author : Junjie Huang
+// Date   : 2021-03-26
+/*******************************************************************************
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ * An input string is valid if:
+ *    Open brackets must be closed by the same type of brackets.
+ *    Open brackets must be closed in the correct order.
+ *
+ * Example 1:
+ *    Input: s = "()"
+ *    Output: true
+ *
+ * Example 2:
+ *    Input: s = "()[]{}"
+ *    Output: true
+ *
+ * Example 3:
+ *    Input: s = "(]"
+ *    Output: false
+ *
+ * Example 4:
+ *    Input: s = "([)]"
+ *    Output: false
+ *
+ * Example 5:
+ *    Input: s = "{[]}"
+ *    Output: true
+ *
+ * Constraints:
+ *    1 <= s.length <= 104
+ *    s consists of parentheses only '()[]{}'.
+ ******************************************************************************/
+
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.size() % 2) return false;
+        stack<char> sta;
+        map<char, char> pairs= {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+        for (char c : s) {
+            if (!sta.empty() && sta.top() == pairs[c]) {
+                sta.pop();
+            } else {
+                sta.push(c);
+            }
+        }
+        return sta.empty();
+    }
+};
